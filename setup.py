5e9b2dafeee318aeb6ab3d3566483967d336ec88
Feng Zhou <zf.pascal@gmail.com>
2014-09-08 09:17:04 +0800

    :retab

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from setuptools import setup
 
 setup(
-	name='nginx-sites',
-	url='https://github.com/zweifisch/nginx-sites',
-	version='0.1.0',
-	description='cli utility for managing nginx site configs',
-	author='Feng Zhou',
-	author_email='zf.pascal@gmail.com',
-	packages=['nginx_sites'],
-	package_data={'nginx_sites':['templates/*.conf']},
-	install_requires=['docopt', 'blessings', 'pystache'],
-	entry_points={
-		'console_scripts': ['nginx-sites=nginx_sites:main'],
-	},
+    name='nginx-sites',
+    url='https://github.com/zweifisch/nginx-sites',
+    version='0.1.0',
+    description='cli utility for managing nginx site configs',
+    author='Feng Zhou',
+    author_email='zf.pascal@gmail.com',
+    packages=['nginx_sites'],
+    package_data={'nginx_sites': ['templates/*.conf']},
+    install_requires=['docopt', 'blessings', 'pystache'],
+    entry_points={
+        'console_scripts': ['nginx-sites=nginx_sites:main'],
+    },
 )
